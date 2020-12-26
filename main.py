43ab8bc8c01fc3ac71333bffa7d8799ecbd8faef
EcoBeys <63933370+EcoBeys@users.noreply.github.com>
2020-12-26 13:54:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import urllib.request
-from sys import argv
+from sys import *
 import urllib3
 from os import system as terminal
 import requests
 from colorama import Fore, Style
-
+import sys
+import time
+sys.path.insert(0,"./pythonProxyGenerator-master/")
+print("""
+      _______  _______  _______
+     (  ____ )(  ____ )(  ___  )|\     /||\     /|
+     | (    )|| (    )|| (   ) |( \   / )( \   / )
+     | (____)|| (____)|| |   | | \ (_) /  \ (_) /
+     |  _____)|     __)| |   | |  ) _ (    \   /
+     | (      | (\ (   | |   | | / ( ) \    ) (
+     | )      | ) \ \__| (___) |( /   \ )   | |
+     |/       |/   \__/(_______)|/     \|   \_/
+        
+% Kodlayan Kişi: Efe İnanç
+% Düzenleyen Kişi: Enes Cört
+% Python Version:3.8.5                        
+""")
 count = input("Oluşturulacak Proxy Sayısı: ")
 proxy_api = "https://www.proxyscan.io/api/proxy?format=txt&type=http"
 
@@ -23,8 +39,17 @@ if __name__ == '__main__':
         with urllib.request.urlopen(proxy_api) as url:
             s = url.read()
             proxy = (s.strip().decode("utf-8"))
-            # print(x, ".", "Proxy")
+            print(proxy)
+            x = proxy        
+            time.sleep(2)
+            with open("Proxy.txt", "a") as file:
+                        file.write(x +"\n")
+            file.close()
+        
     else:
-        print("Finally finished!")
+        print("ProxyLeriniz Hazır!")
+        print("Proxleriniz Başarıyla Dosyaya Kaydedildi")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
---
+++ requirements.bat
@@ -0,0 +1,4 @@
+@echo 
+pip install urllib3
+pip install requests
+pip install colorama
